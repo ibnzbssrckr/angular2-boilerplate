2dd050c0d920c2b95dc563bae8856d4a034d93f6
Michael Geissler <ibnzbssrckr@yahoo.com>
2016-07-10 13:16:14 -0500

    Initial commit

---
+++ dev/main.ts
@@ -0,0 +1,7 @@
+/// <reference path="../typings/index.d.ts" />
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component/app.component';
+
+bootstrap(AppComponent);
